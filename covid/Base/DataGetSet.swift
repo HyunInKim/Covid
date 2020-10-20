970a9094b9f73704879bf3f4a1ae50eed1f558ed
김현인 <kimhyuninn1018@gmail.com>
2020-10-21 01:31:57 +0900

    - 데이터 저장공간

---
+++ covid/Base/DataGetSet.swift
@@ -0,0 +1,31 @@
+//
+//  Data.swift
+//  covid
+//
+//  Created by 김현인 on 2020/10/17.
+//
+
+import Foundation
+
+final class DataGetSet {
+    private init() {}
+    static let shared = DataGetSet()
+    var covidDeathArray:[Int] = []
+    public var covidDeath: Int {
+        get {
+            return defaultForKey(key: "deathCnt")
+        } set(value) {
+            self.covidDeathArray.append(value)
+            setDefault(key: "deathCnt", value: value)
+        }
+    }
+    
+    private func setDefault(key: String, value: Any) -> Void {
+        UserDefaults.standard.set(value, forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+    
+    private func defaultForKey(key: String) -> Int {
+        return UserDefaults.standard.integer(forKey:key)
+    }
+}
